44a35b38dacf9fb29f3ec905904d340ed009fe32
Luis Fernando Chavier <lfchavier@circuitar.com.br>
2013-08-15 14:46:33 -0300

    Updated copyright notice

--- Nanoshield_RTCPlus.cpp
+++ Nanoshield_RTCPlus.cpp
@@ -1,7 +1,7 @@
 /*
 This is the library to access the RTCPlus Nanoshield
 
-Copyright (c) 2013 Luis Fernando Chavier
+Copyright (c) 2013 Circuitar
 This software is released under the MIT license. See the attached LICENSE file for details.
 */
 
